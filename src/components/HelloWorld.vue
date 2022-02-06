99f391f0a30e71405c7994c6fa394c940f590166
Atul Anand <atul_cont16@olacabs.com>
2022-02-07 00:23:35 +0530

    Initital commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  Test 3
+  <aa-hello-world />
+</template>
+
+<script>
+import AaHelloWorld from 'aa-vue-hello-world-simple'
+export default {
+  components: {
+    // OtherComponent
+    AaHelloWorld
+  },
+  data () {
+    return {
+      greeting: 'Hello'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
